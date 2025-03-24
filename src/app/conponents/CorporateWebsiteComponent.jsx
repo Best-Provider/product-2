2151be7a487cc778ab53acc3f94da3fbbf5900eb
Ali105111 <secondslearnin@gmail.com>
2025-03-25 02:46:26 +0500

    Colors Updated

--- src/app/conponents/CorporateWebsiteComponent.jsx
+++ src/app/conponents/CorporateWebsiteComponent.jsx
@@ -65,17 +65,7 @@ const CorporateWebsiteComponent = () => {
         </div>
       </section>
       
-      {/* Scroll to top button */}
-      <div className="fixed bottom-6 right-6">
-        <button 
-          onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
-          className="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
-          </svg>
-        </button>
-      </div>
+
     </div>
   );
 };
